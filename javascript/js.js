0ec70d048c92dd181e44a528488e85320345288a
AnthonyJuho <112909624+AnthonyJuho@users.noreply.github.com>
2022-12-22 20:06:28 +0900

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -1,88 +1,140 @@
-var sun = document.getElementById("sun");
-var earth = document.getElementById("earth");
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+//set control location
 var control = document.getElementById("control");
+control.style.top = (height-100)+"px";
+control.style.left = (width-100)+"px";
 
-const earthinfo = {
-    top: (window.innerHeight/2),
-    left: 400,
-    speed: {
-        down: 2.15,
-        right: 0
+//set close button location
+var close = document.getElementById("close");
+close.style.top = (height-100)+"px";
+close.style.left = (width-100)+"px";
+
+//close button event
+close.addEventListener("click",()=>{
+    close.style.display = "none";
+    control.style.display = "inline";
+    for(const hide of setting.children){
+        hide.style.display = "inline";
+        hide.style.position = "absolute"
     }
-};
-const suninfo = {
-    top: window.innerHeight/2,
-    left: window.innerWidth/2,
-    speed: {
-        down: 0,
-        right: 0
-    }
-}
-const distance = {
-    top:0,
-    left:0,
-    dia:0
-}
-var time = 120;
-var power = 0;
-var startpower = 2000;
-var playing = false;
-
-
-setControl();
-drawNew();
-let Move = setInterval(function() {
-    if(playing){
-        MoveEarth();
-        MoveSun();
-        setEarthInfo();
-        setSunInfo();
-    }
-},1000/time);
-
-control.addEventListener("click", function(event) {
-    if(playing){
-        playing = false;
-    } else {
-        playing = true;
+    for(const show of document.getElementById("planetsetting").children){
+        show.style.display = "none";
     }
 });
 
-function MoveEarth(){
-    distance.top = earthinfo.top-suninfo.top;
-    distance.left = earthinfo.left-suninfo.left;
-    distance.dia = Math.sqrt(Math.pow(distance.top,2)+Math.pow(distance.left,2));
-    power = startpower/Math.pow(distance.dia,2);
+var plus = document.getElementById("+");
+var text = document.getElementById("text");
 
-    distance.top /= distance.dia;
-    distance.left /= distance.dia;
+var setting = document.getElementById("setting");
 
-    distance.top *= power;
-    distance.left *= power;
+//addplanet animation
+var addplanet = document.getElementById("addplanet");
+addplanet.addEventListener("mouseover", () => {
+    plus.style.display = "none";
+    text.style.display = "block";
+});
+addplanet.addEventListener("mouseout", ()=> {
+    plus.style.display = "block";
+    text.style.display = "none";
+});
+//create planet
+text.addEventListener("click", () => {
+    CreatePlanet();
+});
 
-    earthinfo.speed.down += distance.top;
-    earthinfo.speed.right += distance.left;
+var creating = false;
 
-    earthinfo.top -= earthinfo.speed.down;
-    earthinfo.left -= earthinfo.speed.right;
+function CreatePlanet(){
+    if(!creating){
+        var planet = document.createElement("div");
+        planet.classList.add("manage");
+        planet.id = "create";
+
+        var input = document.createElement("input");
+        input.type = "text";
+        input.id = "input";
+        input.classList.add("input");
+
+        setting.append(planet);
+        planet = document.getElementById("create");
+        planet.append(input);
+        creating = true;
+        inputListener();
+    }
+
 }
-function MoveSun(){
-    suninfo.top += suninfo.speed.down;
-    suninfo.left += suninfo.speed.right;
+//set planet name
+function inputListener(){
+    var input = document.getElementById("input");
+    input.addEventListener("change", (event)=> {
+        var planet = document.getElementById("create");
+        var name = input.value;
+        var p = document.createElement("p");
+        var content = document.createTextNode(name);
+        p.appendChild(content);
+        planet.append(p);
+        planet.id = name;
+        input.remove();
+        creating = false;
+        planet.addEventListener("mouseover", () => {
+            planet.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        });
+        planet.addEventListener("mouseout", ()=> {
+            planet.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+        });
+        planet.addEventListener("click", ()=>{
+            ManagePlanetInformation(planet);
+        });
+        CreatePlanetDiv(name);
+    });
 }
-function setEarthInfo(){
-    earth.style.top = earthinfo.top+"px";
-    earth.style.left = earthinfo.left+"px";
+function ManagePlanetInformation(planet){
+    //hide another options
+    for(const hide of setting.children){
+        if(hide.id != planet.id){
+            hide.style.display = "none";
+        } else {
+            hide.style.position = "relative";
+        }
+    }
+    control.style.display = "none";
+    close.style.display = "inline";
+    for(const show of document.getElementById("planetsetting").children){
+        show.style.display = "block";
+    }
 }
-function setSunInfo(){
-    sun.style.top = suninfo.top+"px";
-    sun.style.left = suninfo.left+"px";
+function CreatePlanetDiv(name){
+    var planet = document.createElement("div");
+    planet.classList.add("planet");
+    planet.id = name;
+    document.getElementById("space").append(planet);
 }
-function drawNew(){
-    setEarthInfo();
-    setSunInfo();
+
+var playing = false;
+//control function
+control.addEventListener("mouseover", ()=> {
+    control.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+});
+control.addEventListener("mouseout", ()=>{
+    control.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+});
+control.addEventListener("click", ()=>{
+    var controlvalue = document.getElementById("controlvalue");
+    if(playing) {
+        playing = false;
+        controlvalue.innerText = "Start";
+        ShowManage();
+    } else {
+        playing = true;
+        controlvalue.innerText = "Stop";
+        HideManage();
+    }
+});
+function HideManage(){
+    setting.style.display = "none";
 }
-function setControl(){
-    control.style.top = (window.innerHeight-100)+"px";
-    control.style.left = (window.innerWidth-100)+"px";
+function ShowManage(){
+    setting.style.display = "inline";
 }
